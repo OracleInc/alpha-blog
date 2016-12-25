79ef6c8f70ff3051d1b186d9d53343831a2be00d
makena <roselynemakena@gmail.com>
2016-12-25 22:55:36 +0300

    Added gravatar for profile page and filter for articles

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+before_action :get_user, only:[:edit, :update, :show]
+  def new
+    @user = User.new
+    
+  end
+
+  def show
+
+    
+  end
+
+  def create
+
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "User created successfully. Welcome #{user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+    
+  end
+  def edit
+
+  end
+  def update
+    if @user.update(user_params)
+      flash[:success] = "User credentials successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+    
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+
+  end
+  def get_user
+    @user = User.find(params[:id])
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
   resources :articles
+  resources :users, except: [:new]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
