4f56b2744e4d55aa774f23f97b6f1a60e1e468b2
makena <roselynemakena@gmail.com>
2016-12-26 17:54:05 +0300

    added users listing and added users show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 before_action :get_user, only:[:edit, :update, :show]
+
+def index
+  @users = User.all
+  
+end
   def new
     @user = User.new
     
